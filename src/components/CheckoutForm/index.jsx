0ab7a58a56249e8592bccb8c767bb979fb230dac
Nona <nonajuwita2023@gmail.com>
2025-01-29 00:50:57 +0900

    Add Usenavigate CheckoutForm

--- src/components/CheckoutForm/index.jsx
+++ src/components/CheckoutForm/index.jsx
@@ -1,9 +1,8 @@
-// components/CheckoutForm.jsx
-import React, { useState } from "react";
-import PaymentMethod from "./PaymentMethod";
+import { useNavigate } from "react-router-dom";
 
-const CheckoutForm = ({ cartItems, onTransactionSuccess }) => {
+const CheckoutForm = ({ cartItems }) => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+  const navigate = useNavigate();
 
   const handleCheckout = async () => {
     if (!selectedPaymentMethod) {
@@ -19,7 +18,7 @@ const CheckoutForm = ({ cartItems, onTransactionSuccess }) => {
           headers: {
             apiKey: "24405e01-fbc1-45a5-9f5a-be13afcd757c",
             Authorization:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5vbmE0QGdtYWlsLmNvbSIsInVzZXJJZCI6IjcyMzBkMmMyLTU0ZjEtNDZhNS04ZTQ4LWQ2YjQ3ZGEyN2M4NCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzM3NDQ5Njc2fQ.SZlMq8VYrBkWDAGQF2FeWagqpMJ4QohLtFgm5_hon3w",
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5vbmE0QGdtYWlsLmNvbSIsInVzZXJJZCI6IjcyMzBkMmMyLTU0ZjEtNDZhNS04ZTQ4LWQ2YjQ3ZGEyN2M4NCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzM3NDQ5Njc2fQ.SZlMq8VYrBkWDAGQF2FeWagqpMJ4QohLtFgm5_hon3w",
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
@@ -38,7 +37,7 @@ const CheckoutForm = ({ cartItems, onTransactionSuccess }) => {
 
       const data = await response.json();
       alert("Transaction successful!");
-      onTransactionSuccess(data); // Inform parent component
+      navigate("/transactions"); // Arahkan ke halaman transaksi
     } catch (err) {
       console.error(err);
       alert("Transaction failed. Please try again.");
@@ -47,6 +46,7 @@ const CheckoutForm = ({ cartItems, onTransactionSuccess }) => {
 
   return (
     <div className="p-4">
+      {/* Payment Method Selection */}
       <PaymentMethod onSelect={setSelectedPaymentMethod} />
       <button
         className="px-6 py-3 mt-4 text-white bg-green-500 rounded-lg"
@@ -57,5 +57,3 @@ const CheckoutForm = ({ cartItems, onTransactionSuccess }) => {
     </div>
   );
 };
-
-export default CheckoutForm;
