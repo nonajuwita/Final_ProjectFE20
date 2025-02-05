e7f5247950e2170bec7be5ef257f2598f79e8410
Nona <nonajuwita2023@gmail.com>
2025-02-06 07:55:52 +0900

    Update AuthContexs, auth role

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -8,28 +8,37 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [role, setRole] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+    const userRole = localStorage.getItem('role');
+
     if (token) {
       setIsAuthenticated(true);
+      setRole(userRole);
     } else {
       setIsAuthenticated(false);
+      setRole(null);
     }
   }, []);
 
-  const login = (token) => {
-    localStorage.setItem('token', token); // Simpan token di localStorage
-    setIsAuthenticated(true); // Set status login ke true
+  const login = (token, userRole) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('role', userRole);
+    setIsAuthenticated(true);
+    setRole(userRole);
   };
 
   const logout = () => {
-    localStorage.removeItem('token'); // Hapus token dari localStorage
-    setIsAuthenticated(false); // Set status login ke false
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    setIsAuthenticated(false);
+    setRole(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, role, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
